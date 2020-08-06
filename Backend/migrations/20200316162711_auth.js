04b78593f88af12729693f6cdefb01136ee3642c
Michael Famurewa <myfamurewa@gmail.com>
2020-08-05 23:13:27 -0400

    added the migration tables

--- Backend/migrations/20200316162711_auth.js
+++ Backend/migrations/20200316162711_auth.js
@@ -7,6 +7,31 @@ exports.up = function(knex) {
         tbl.string('email').notNullable();
         tbl.string('password').notNullable();
       })
+    .createTableIfNotExists('toGameList', tbl => {
+      tbl.increments()
+      tbl.string("name")
+      tbl.integer("length")
+      tbl.string("genre")
+      tbl.boolean("sequel").defualtTo(false)
+      tbl.integer("user_Id")
+       .unsigned()
+       .notNullable()
+       .reference('id')
+       .inTable('users')
+       .onDelete('CASCADE')
+    })
+    .createTableIfNotExists('greatestHits', tbl => {
+      tbl.increments()
+      tbl.string("name")
+      tbl.string("genre")
+      tbl.integer("length")
+      tbl.integer("user_Id")
+       .unsigned()
+       .notNullable()
+       .reference('id')
+       .inTable('users')
+       .onDelete('CASCADE')
+    })
 };
 
 exports.down = function(knex) {
